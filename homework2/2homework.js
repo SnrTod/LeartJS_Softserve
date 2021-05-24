235b27060f0b04813b84e22ccfd29a6edd4eb407
SnrTod <sandra.todoruk@gmail.com>
2021-05-24 15:05:35 +0300

    homework 2

---
+++ homework2/2homework.js
@@ -0,0 +1,102 @@
+// task 1
+// 
+// let x = 1;
+// let y = 2;
+
+// let res1 = x + '' + y; 
+// console.log(res1); 
+// console.log(typeof res1);
+// 
+// let res2 = Boolean(x) + String(y);
+// console.log(res2);
+// console.log(typeof res2); 
+// 
+// let res3 = Boolean(x); /*або !!x;*/
+// console.log(res3); 
+// console.log(typeof res3); 
+
+// let res4 = parseInt("x" + "y");
+// console.log(res4); 
+// console.log(typeof res4);
+
+
+// task 2
+
+// const number = parseInt(prompt("Please, enter number"));
+// if (number > 0) {
+//     console.log("Number " + number + " positive");
+// }
+// else {
+//         console.log("Number " + number + " negative");
+// };
+// if (number % 7 === 0) {
+//         console.log("The number " + number + " is a multiple of 7");
+// }
+//     console.log(typeof number);
+//     
+    
+// task 3
+
+// let new_arr = [7,"Lviv", true, null];
+// console.log(new_arr.length);
+// let new_elements_arr = prompt ("enter any value");
+// new_arr.push(new_elements_arr);
+// console.log(new_arr[4]);
+// console.log(new_arr.shift(0), new_arr);
+
+
+// task 4
+
+// let cities = ["Rome", "Lviv", "Warsaw"]; 
+// let new_elements_arr = '*';
+// console.log(cities.join(new_elements_arr));
+
+// task 5
+
+// const min_age = 10;
+// let isAdult = prompt("How old are you?");
+// if (isAdult >= 18) {
+//     alert("Ви досягли повнолітнього віку");
+// }
+// else if (isAdult >= min_age) {
+//         alert("Ви ще неповнолітний/ня");
+// }
+// else if (isAdult < min_age) {
+//         alert("Ви ще надто молоді");
+// }
+
+// task 6
+
+// let a = +prompt("введіть довжину сторони а");
+// let b = +prompt("введіть довжину сторони b");
+// let c = +prompt("введіть довжину сторони c");
+// let p = (a + b + c) / 2;
+// let s_triangle = p * (p - a) * (p - b) * (p - c);
+// 
+// console.log("Площа даного трикутника " + Math.sqrt(s_triangle));
+// 
+
+// task 7
+
+// let time = new Date();
+// let hours = time.getHours()
+// let minutes = time.getMinutes();
+// 
+// console.log(hours, minutes);
+//  if (hours >= 23 && hours <=5 ) {
+//     alert("Доброї ночі!")
+// }
+// else if (hours >= 5 && hours <=11 ) {
+//     alert("Доброго ранку!")
+// }
+// else if (hours >= 11 && hours <=17 ) {
+//     alert("Доброго дня!")
+// }
+// else if (hours >= 17 && hours <=23 ) {
+//     alert("Доброго вечора!")
+// }
+
+
+
+
+
