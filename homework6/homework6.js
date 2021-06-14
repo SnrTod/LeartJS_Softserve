509ac2e699c5b985e325830e422374962dc91eb8
SnrTod <sandra.todoruk@gmail.com>
2021-06-14 14:28:11 +0300

    homework6

---
+++ homework6/homework6.js
@@ -0,0 +1,23 @@
+// -task 1-
+// 1)
+// let zminy = document.getElementById("test").innerHTML = "Last";
+// 2)
+// let zminy2 = document.getElementsByTagName("div").innerHTML = "Last"; ---???
+
+// -task 2-
+
+// let dog = document.getElementsByClassName("image").src ="dog.jpg" ---??
+
+// -task 3-
+
+
+
+// -task 4-
+// -task 5-
+let change_h1 = document.getElementsByTagName("h1");
+for (let i=0; i<h1.length; i++) {
+change_h1[i].style.fontSize = '30px';
+   change_h1[i].style.backgroundColor = 'grey';
+  }
+// -task 6-
+// -task 7-
